59f0147f6c264548d85fb8e8cfa8b53d7d196a77
Shaktigupta97 <shaktigupta321@gmail.com>
2020-09-18 17:53:55 +0530

    First Build

--- assets/js/dapp.js
+++ assets/js/dapp.js
@@ -10,31 +10,34 @@ let gameID;
 let randomness;
 let startGameTXHash;
 
+
 // Inject our version of web3.js into the DApp.
 window.addEventListener('load', async() => {
-    // Modern dapp browsers...
-    if (window.ethereum) {
-        window.web3 = new Web3(ethereum);
-        try {
-            // Request account access if needed
-            await ethereum.enable();
-            // Acccounts now exposed
-            startDApp();
-            web3.eth.sendTransaction({ /* ... */ });
-        } catch (error) {
-            // User denied account access...
-        }
-    }
-    // Legacy dapp browsers...
-    else if (window.web3) {
-        window.web3 = new Web3(web3.currentProvider);
-        // Acccounts always exposed
-        web3.eth.sendTransaction({ /* ... */ });
-    }
-    // Non-dapp browsers...
-    else {
-        console.log('Non-Ethereum browser detected. Install MetaMask to continue!');
-    }
+  $('#main-popup').modal('show');
+
+  // Modern dapp browsers...
+  if (window.ethereum) {
+      window.web3 = new Web3(ethereum);
+      try {
+          // Request account access if needed
+          await ethereum.enable();
+          // Acccounts now exposed
+          startDApp();
+          web3.eth.sendTransaction({ /* ... */ });
+      } catch (error) {
+          // User denied account access...
+      }
+  }
+  // Legacy dapp browsers...
+  else if (window.web3) {
+      window.web3 = new Web3(web3.currentProvider);
+      // Acccounts always exposed
+      web3.eth.sendTransaction({ /* ... */ });
+  }
+  // Non-dapp browsers...
+  else {
+      console.log('Non-Ethereum browser detected. Install MetaMask to continue!');
+  }
 })
 
 function checkNetwork() {
